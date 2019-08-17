686575c771ba8362abff962fef726a04e227a478
Brandon Valley <bavvh8@mst.edu>
2019-08-17 17:29:07 -0500

    added stuff from many_mini, in middle of adding s for start command

---
+++ scripts/init_submit.ps1
@@ -0,0 +1,248 @@
+﻿
+Set-ExecutionPolicy RemoteSigned
+
+<#
+
+param ( [parameter(Mandatory=$true)] [string] $first_name,
+        [parameter(Mandatory=$true)] [String] $last_name,
+        [parameter(Mandatory=$true)] [String] $email,
+        [parameter(Mandatory=$true)] [String] $zip)
+        #>
+
+#miliseconds
+$DEFAULT_WAIT_TIME = 100 
+$OPEN_URL_WAIT_TIME = 2500
+
+$BRAVE_SHORTCUT_PATH = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Brave.lnk"
+$FOUNDATION_URL = "https://googlenest.reevefoundation.org"
+
+
+$CONFIG_FILE_PATH = "C:\Users\Brandon\Documents\Personal_Projects\many_mini\scripts\init_submit\init_sumbit_data.csv"
+$FAILED_EMAILS_PATH = "C:\Users\Brandon\Documents\Personal_Projects\many_mini\scripts\init_submit\failed_emails.txt"
+$NUM_TABS_TO_LINK = 19
+$NUM_TABS_TO_FIRST_RAD_BTN_D = @{"chrome"    = 23 #12:41 - 1st time hit 2:44
+                                 "firefox"   = 29
+                                 "opera"     = 23
+                                 "edge"      = 23 # :(
+                                 "brave"     = 23 # caught 1st try 3:04
+                                 "icedragon" = 29
+                                 "Maxthon (32 bit)" = 23 # :( - hit 1st time with notepad
+                                 "tor browser"      = 29
+                                 "slimbrowser"      = 29
+                                 "notepad"           = 31
+                                 "internet explorer" = 33} # got foucus once but never again, caught on 2nd try with notepad
+
+
+function set_up_next_round
+{
+    cmd /c $BRAVE_SHORTCUT_PATH
+
+
+    start-sleep -milliseconds 500 #$OPEN_URL_WAIT_TIME
+
+    $wshell = New-Object -ComObject wscript.shell;
+    $wshell.AppActivate($browser)
+    #$wshell.AppActivate('notepad')
+
+
+
+
+
+
+    $wshell.SendKeys("$FOUNDATION_URL")
+    start-sleep -milliseconds $DEFAULT_WAIT_TIME
+    $wshell.SendKeys("{ENTER}")
+
+}
+
+
+
+
+# must start from https://googlenest.reevefoundation.org/ after getting to it by clicking link on
+# https://www.christopherreeve.org/about-us/press-releases/google-nest-partners-with-the-christopher-dana-reeve-foundation-to-improve-independence-for-individuals-living-with-paralysis
+
+function init_submit_1_acc
+{
+    param ( [parameter(Mandatory=$true)] [string] $first_name,
+            [parameter(Mandatory=$true)] [String] $last_name,
+            [parameter(Mandatory=$true)] [String] $email,
+            [parameter(Mandatory=$true)] [String] $zip)
+
+    $browser = "brave"
+
+
+
+
+
+    try
+    {
+     
+        #$wshell = New-Object -ComObject wscript.shell;
+        #$wshell.AppActivate('Chrome')
+        #$wshell.AppActivate($browser)
+        #$wshell.AppActivate('notepad')
+
+        #sleep .5
+
+
+
+        # for unknown browser, activate notepad and click on the browser befor the delay is up
+        # might only be for Yandex but need to use tabs to get to link, not click, or else 
+        # you cant open the page again with ENTER
+        if ($browser -eq "notepad")
+        {
+            $wshell.SendKeys("about to start")
+            start-sleep -milliseconds 1000
+
+        }
+
+
+        #close current tab then open a new one from the link
+        start-sleep -milliseconds 1000 # not sure if this needs to be longer than default
+        $wshell.SendKeys("^{w}") #close cur tab
+        start-sleep -milliseconds $DEFAULT_WAIT_TIME
+        $wshell.SendKeys("{ENTER}")
+        start-sleep -milliseconds $OPEN_URL_WAIT_TIME # need to wait a bit longer for page to load
+        #start-sleep 50000 #`````````````````````````````````````````````````````````````````````````````````````
+
+
+        
+        # get to first radio btn
+        For ($i=0; $i -le $NUM_TABS_TO_FIRST_RAD_BTN_D[$browser] ; $i++) {
+    
+            $wshell.SendKeys("{TAB}")
+            #$wshell.SendKeys("A")
+            #Write-Output "sent tab"
+            start-sleep -milliseconds $DEFAULT_WAIT_TIME
+            #sleep .1
+
+            #Start-Sleep -s 0.5
+
+            }
+
+
+        #check radio btns, end on 1st text box, end on 1st rad btn to skip
+        For ($i=0; $i -le 2; $i++) 
+        {
+            $wshell.SendKeys(" ")
+            #$wshell.SendKeys("A")
+            start-sleep -milliseconds 300
+            $wshell.SendKeys("{TAB}")
+            start-sleep -milliseconds 300
+        }
+
+        #fill text boxes
+        $wshell.SendKeys($first_name)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+        $wshell.SendKeys($last_name)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+        $wshell.SendKeys($email)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+        $wshell.SendKeys($zip)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+
+        # skip then check I have read req rad btn
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+        $wshell.SendKeys(" ")
+        start-sleep -milliseconds 300
+
+
+        # hit sumbit btn
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{ENTER}")
+
+
+    }
+    catch [System.Exception]
+    {
+        Write-Both -Foreground "Red" "$($_.Exception.GetType().FullName)"
+        Write-Both -Foreground "Red" $($MyInvocation.MyCommand.ToString() + " line " +  $MyInvocation.ScriptLineNumber.ToString()`
+                   + " " + [string]$Error[0])
+    }
+}
+
+
+
+
+
+
+function read_init_submit_data
+{
+        $row_l = @()
+        $configCSV = import-csv $CONFIG_FILE_PATH
+
+        ForEach( $row in $configCSV )
+        {
+            $row_l += $row
+        }
+        return $row_l
+}
+
+
+
+
+
+
+
+
+
+$row_l = read_init_submit_data
+Write-Output $row_l[0]
+$row_num = 202
+
+while($true)
+{
+    $user_input = Read-Host
+    
+    if ($user_input -eq 'f')
+    {
+        Add-Content $FAILED_EMAILS_PATH $row_l[$row_num - 1].email 
+        Write-Output "added email to fail file  " $row_l[$row_num - 1].email 
+    }
+    else
+    {
+        if ($user_input -eq 's')
+        {
+            set_up_next_round
+            exit #```````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+        }
+        Write-Host $row_l[$row_num].email
+        write-host $row_num
+        init_submit_1_acc $row_l[$row_num].first_name $row_l[$row_num].last_name $row_l[$row_num].email $row_l[$row_num].zip
+        $row_num += 1
+    }
+
+
+
+    
+}
+
+
+#Write-Output $row_l
+
+
+
+
+
+#init_submit_1_acc "bob" "man" "111@gmail.com" "10001"
+#init_submit_1_acc $first_name $last_name $email $zip
---
+++ scripts/init_submit/init_submit.ps1
@@ -0,0 +1,114 @@
+﻿
+
+
+
+$DEFAULT_WAIT_TIME = 100 #miliseconds
+
+
+
+function init_submit_1_acc
+{
+    param ( [parameter(Mandatory=$true)] [string] $first_name,
+            [parameter(Mandatory=$true)] [String] $last_name,
+            [parameter(Mandatory=$true)] [String] $email,
+            [parameter(Mandatory=$true)] [String] $zip)
+
+    try
+    {
+     
+        $wshell = New-Object -ComObject wscript.shell;
+        $wshell.AppActivate('Chrome')
+        #$wshell.AppActivate('notepad')
+
+        #sleep .5
+
+        # get to first radio btn
+        For ($i=0; $i -le 23; $i++) {
+    
+            $wshell.SendKeys("{TAB}")
+            #$wshell.SendKeys("A")
+            #Write-Output "sent tab"
+            start-sleep -milliseconds $DEFAULT_WAIT_TIME
+            #sleep .1
+
+            #Start-Sleep -s 0.5
+
+            }
+
+
+        #check radio btns, end on 1st text box, end on 1st rad btn to skip
+        For ($i=0; $i -le 2; $i++) 
+        {
+            $wshell.SendKeys(" ")
+            #$wshell.SendKeys("A")
+            start-sleep -milliseconds 300
+            $wshell.SendKeys("{TAB}")
+            start-sleep -milliseconds 300
+        }
+
+        #fill text boxes
+        $wshell.SendKeys($first_name)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+        $wshell.SendKeys($last_name)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+        $wshell.SendKeys($email)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+        $wshell.SendKeys($zip)
+
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+
+
+        # skip then check I have read req rad btn
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+        $wshell.SendKeys(" ")
+        start-sleep -milliseconds 300
+
+
+        # hit sumbit btn
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{TAB}")
+        start-sleep -milliseconds 300
+        $wshell.SendKeys("{ENTER}")
+
+
+    
+
+
+
+    }
+    catch [System.Exception]
+    {
+        Write-Both -Foreground "Red" "$($_.Exception.GetType().FullName)"
+        Write-Both -Foreground "Red" $($MyInvocation.MyCommand.ToString() + " line " +  $MyInvocation.ScriptLineNumber.ToString()`
+                   + " " + [string]$Error[0])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+init_submit_1_acc "bob" "man" "111@gmail.com" "10001"
+
